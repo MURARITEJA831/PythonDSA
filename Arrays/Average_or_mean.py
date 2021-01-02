705afa9194bad13f1ba8ff416b763f98c935129a
MURARITEJA831 <murariteja831@gmail.com>
2021-01-02 15:16:02 +0530

    1st programme added

--- Arrays/Average_or_mean.py
+++ Arrays/Average_or_mean.py
@@ -1,1 +1,9 @@
-print('Hello world')
+def average(l):
+    sum = 0
+    for i in range(len(l)):
+        sum += l[i]
+    return sum/len(l)
+
+n = int(input('Enter length of the list: '))
+l = list(map(int,input('Enter items: ').split()))[:n]
+print(average(l))
