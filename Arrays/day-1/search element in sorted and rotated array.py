3a1fd2e8105077d1a6cb80a0e8fd48b834a9d66b
MURARITEJA831 <murariteja831@gmail.com>
2021-01-10 17:42:35 +0530

    pairsum with pivot element

--- Arrays/day-1/search element in sorted and rotated array.py
+++ Arrays/day-1/search element in sorted and rotated array.py
@@ -1,4 +1,6 @@
-
+# method-1
+# O(Logn)
+# O(1)
 
 def findpivot(arr,low,high):
     if high < low:
@@ -41,3 +43,30 @@ low = 0
 high = len(arr) - 1
 x = 5
 print('Index of the ',x,' is ',pivotedBinarySearch(arr,low,high,x))
+
+# improvised solution with one traversal
+# O(logn)
+# O(1)
+
+def search(arr,low,high,x):
+    if low > high:
+        return -1
+    if low == high:
+        return low
+    mid = (low+high)//2
+    if arr[mid] == x:
+        return mid
+    if arr[low] <= arr[mid]:
+        if x >= arr[low] and x <= arr[mid]:
+            return search(arr,low,mid-1,x)
+        return search(arr,mid+1,high,x)
+    if x >= arr[mid] and x <= arr[high]:
+        return search(arr,mid+1,high,x)
+    return search(arr,low,mid-1,x)
+
+
+arr = [-1,-2,-3,1,2,3,4,5]
+low = 0
+high = len(arr)-1
+x = 3
+print('position of ',x,' is ',search(arr,low,high,x))
