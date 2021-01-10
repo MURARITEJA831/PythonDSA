d72e915c3fd05d1a4d640068eb21ca810de8a90b
MURARITEJA831 <murariteja831@gmail.com>
2021-01-10 09:11:50 +0530

    3-methods added

---
+++ Arrays/day-1/array_rotation.py
@@ -0,0 +1,108 @@
+# method-1 (Using temp variable)
+# Time complexity is O(n)
+# space complexity is O(d)
+
+
+def rotation(arr,d,n):
+    temp = []
+    k = n-int(d)
+    for i in range(d):
+        temp.append(arr[i])
+    for i in range(k):
+        arr[i] = arr[i+d]
+    for i in range(d):
+        arr[k+i] = temp[i]
+
+    return arr
+
+arr = [1,2,3,4,5,6]
+d = 5
+n = len(arr)
+rotation(arr,d,n)
+print('The 1st method output ',arr)
+print()
+print()
+
+
+# method-2 (Rotate one by one)
+# time complexity is O(n*d)
+# space complexity is O(1)
+
+def onebyone(arr, d, n):
+    for i in range(d):
+        leftrotatebyone(arr,n)
+
+
+def leftrotatebyone(arr,n):
+    temp = arr[0]
+    for i in range(n-1):
+        arr[i] = arr[i+1]
+    arr[n-1] = temp
+
+
+
+
+
+
+if __name__ == '__main__':
+    arr = [1,2,3,4,5,6]
+    n = len(arr)
+    d = int(input('Enter how many digits you want rotate: '))
+    onebyone(arr,d,n)
+    print('2nd method output ',arr)
+    print()
+    print()
+
+
+# method-3 (Juggling algorithm)
+# time compelxity O(n)
+# auxilary space O(1)
+
+def leftrotate(arr,d,n):
+    d = d%n
+    g = gcd(d,n)
+    for i in range(g):
+        temp = arr[i]
+        j = i
+        while True:
+            k = j + d
+            if k >= n:
+                k = k - n
+            if k == i:
+                break
+            arr[j] = arr[k]
+            j = k
+        arr[j] = temp
+
+
+def gcd(a,b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
+
+
+
+if __name__ == '__main__':
+    arr = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    d = 3
+    n = len(arr)
+    leftrotate(arr,d,n)
+    print('third method output is ',arr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
